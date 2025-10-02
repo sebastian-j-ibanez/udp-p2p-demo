239a2a8a1210e0668a7b198497c87e128a3c5c7d
Sebastian Ibanez <sebas.ibanez219@gmail.com>
2025-10-02 00:09:55 -0400

    fix: refactor protocol, use single client socket, ensure consistent handshake order

--- main.go
+++ main.go
@@ -4,15 +4,17 @@ import (
 	"fmt"
 	"math/rand"
 	"net"
+	"strconv"
+	"time"
 )
 
 func main() {
-	a, err := NewClient(rand.Intn(100))
+	client, err := NewClient(rand.Intn(10000))
 	if err != nil {
 		panic(fmt.Sprintf("Unable to init client: %s", err.Error()))
 	}
 
-	a.RunClient()
+	client.Run()
 }
 
 const DefaultPort = 50000
@@ -27,7 +29,7 @@ func NewClient(id int) (Client, error) {
 		IP:   net.IPv4zero,
 		Port: DefaultPort,
 	}
-	client, err := net.DialUDP("udp", nil, &addr)
+	client, err := net.ListenUDP("udp", &addr)
 	if err != nil {
 		fmt.Printf("error: %s", err.Error())
 		return Client{}, nil
@@ -37,63 +39,110 @@ func NewClient(id int) (Client, error) {
 }
 
 // Run main client loop
-func (c *Client) RunClient() error {
+func (c *Client) Run() error {
 	defer c.Conn.Close()
 
 	stopCast := make(chan bool)
 	go c.Broadcast(stopCast)
 
 	// Try to read from Socket
-	var msg string
-	_, addr, err := c.Conn.ReadFromUDP([]byte(msg))
+	msg := make([]byte, 64)
+	n, addr, err := c.Conn.ReadFromUDP(msg)
 	if err != nil {
 		return err
 	}
 
-	// Stop broadcasting and close socket when
-	// connection is made.
+	peerId, err := strconv.Atoi(string(msg[:n]))
+	if err != nil {
+		return err
+	}
+
+	// Stop broadcasting when connection is made
 	stopCast <- true
-	c.Conn.Close()
 
-	c.Conn, err = net.DialUDP("udp", nil, addr)
-	if err != nil {
-		return err
+	// Check to see which
+	if peerId < c.Id {
+		c.Initiate(addr)
+	} else {
+		c.Respond(addr)
 	}
 
+	return nil
+}
+
+func (c *Client) Initiate(addr *net.UDPAddr) error {
 	// Send ACK to peer
-	msg = fmt.Sprintf("ACK %d", c.Id)
-	_, err = c.Conn.Write([]byte(msg))
+	msg := make([]byte, 64)
+	msg = fmt.Appendf(msg, "ACK %d", c.Id)
+	_, err := c.Conn.WriteToUDP(msg, addr)
 	if err != nil {
 		return err
 	}
 
 	// Receive ACK from peer
-	msg = ""
-	_, err = c.Conn.Read([]byte(msg))
+	clear(msg)
+	_, _, err = c.Conn.ReadFromUDP(msg)
 	if err != nil {
 		return err
 	}
 
 	// Send data to peer
-	msg = "secret message"
-	_, err = c.Conn.Write([]byte(msg))
+	clear(msg)
+	msg = fmt.Appendf(msg, "secret message from client %d", c.Id)
+	_, err = c.Conn.WriteToUDP(msg, addr)
 	if err != nil {
 		return err
 	}
 
 	// Receive data from peer
-	msg = ""
-	_, err = c.Conn.Read([]byte(msg))
+	clear(msg)
+	n, _, err := c.Conn.ReadFromUDP(msg)
 	if err != nil {
 		return err
 	}
-	fmt.Printf("Received: \"%s\"\n", msg)
+	fmt.Printf("Received: \"%s\"\n", string(msg[:n]))
+
+	return nil
+}
+
+func (c *Client) Respond(addr *net.UDPAddr) error {
+	// Receive ACK from peer
+	msg := make([]byte, 64)
+	_, _, err := c.Conn.ReadFromUDP(msg)
+	if err != nil {
+		return err
+	}
+
+	// Send ACK to peer
+	clear(msg)
+	msg = fmt.Appendf(msg, "ACK %d", c.Id)
+	_, err = c.Conn.WriteToUDP(msg, addr)
+	if err != nil {
+		return err
+	}
+
+	// Receive data from peer
+	clear(msg)
+	n, _, err := c.Conn.ReadFromUDP(msg)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Received: \"%s\"\n", string(msg[:n]))
+
+	// Send data to peer
+	clear(msg)
+	msg = fmt.Appendf(msg, "secret message from client %d", c.Id)
+	_, err = c.Conn.WriteToUDP(msg, addr)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
 
 // Broadcast data until stop flag is received
 func (c *Client) Broadcast(stopCast chan bool) {
+	broadcastAddr := &net.UDPAddr{IP: net.IPv4bcast, Port: DefaultPort}
 	for {
 		select {
 		case stop := <-stopCast:
@@ -101,11 +150,12 @@ func (c *Client) Broadcast(stopCast chan bool) {
 				return
 			}
 		default:
-			data := fmt.Sprintf("CLT %d", c.Id)
-			_, err := c.Conn.Write([]byte(data))
+			data := []byte(strconv.Itoa(c.Id))
+			_, err := c.Conn.WriteToUDP(data, broadcastAddr)
 			if err != nil {
 				fmt.Printf("error: %s", err.Error())
 			}
 		}
+		time.Sleep(time.Millisecond * 500)
 	}
 }
